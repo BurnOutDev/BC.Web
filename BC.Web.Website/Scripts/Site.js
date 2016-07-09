091b11b59cc5ced9bb7e7f3058690c234b557513
Irakl <Irakl@DESKTOP-MGCLNGV>
2016-07-09 16:34:06 +0400

    Add project files.

---
+++ BC.Web.Website/Scripts/Site.js
@@ -0,0 +1,30 @@
+﻿$('body').css('min-height', $(window).height() + 'px');
+$('.main-row').css('min-height', $(window).height() - 90 -15 + 'px');
+
+function changeText(jumbotron, header, text, button, ms) {
+    var h1 = $(jumbotron).children('h1');
+    var p = $(jumbotron).children('p')[0];
+    var a = $(jumbotron).children('p').children('a');
+
+    $(h1).fadeOut(ms, function () {
+        $(this).text(header).fadeIn(ms);
+    });
+    $(p).fadeOut(ms, function () {
+        $(this).text(text).fadeIn(ms);
+    });
+    $(a).fadeOut(ms, function () {
+        $(this).html(button).fadeIn(ms);
+    });
+}
+
+$("#next").click(function () {
+    $('.main-row').animate({ scrollLeft: $(window).width() }, { duration: 500, queue: false });
+    $('body').css("background-image", "url(../svg/bg/bg-invert.svg)");
+    $('body').animate({
+        'background-position-x': '-100%',
+        'background-color': '#FFF'
+    }, { duration: 500, queue: false });
+
+    $('body').addClass('bg-color');
+    return false;
+});
